683fad4a28f7a6c14d3ff04457c20ebe41672cb0
brendan-kellam <bshizzle1234@gmail.com>
2017-01-01 18:05:39 -0500

    Added auto-scrolling

--- resume/static/js/parser.js
+++ resume/static/js/parser.js
@@ -17,13 +17,14 @@ function getFileContents(file, callback) {
 }
 
 
-
+var speed = 1;
 function frameLooper(array, element) {
 
  	var timer;
  	var temp
 
 	if (array.length > 0){
+		element.scrollTop = element.scrollHeight;
 		temp = array.shift();
 
 		switch(temp) {
@@ -39,55 +40,58 @@ function frameLooper(array, element) {
 
 	}else{
 		clearTimeout(timer);
+		//console.log("here");
+		//return;
 	}
 
-	timer = setTimeout(frameLooper.bind(null, array, element), 0);
+	timer = setTimeout(frameLooper.bind(null, array, element), speed);
+
+	//timer = setTimeout(frameLooper.bind(null, array, element), 1);
 
 }
 
 
+/* formatStr: accepts raw string and converts to a "screen-drawable" format */
 function formatStr(str){
-
 
 	var output = "";
 	var line = "";
+
+	// loop each character of accepted string
 	for (var i = 0, len = str.length, c = ""; i < len; i++){
 		c = str[i];
-		
+			
+		// switch-case on a given character
 		switch(c){
-			case '\n':
+			case '\n': // new-line
 				line += '^';
 				output += line;
 				line = "";
 				break;
 
-			case '\t':
-				line += "~~~~~~~~";
+			case '\t': // tab
+				line += "~~~~~~~~"; // NOTE: A '~' character indicates a space
 				break;
 
-			default:
+			default: // default behavior
 				line += c;
 		}
 
 	}
 
-
+	// return formated string
 	return output;
 }
 
 
 function writeOut(file, element){
 	
-	getFileContents("", function(str) {
+	getFileContents(file, function(str) {
 		var output = formatStr(str);	
 	
 		frameLooper(output.split(""), element)		
 
 	});
 }
-
-
-
-
 
 	
